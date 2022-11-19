0bb3a9ffdd2038711b3d615997ca3d1e72c8551c
Swann HERRERA <swann@devloup.dev>
2022-11-19 10:38:52 +0100

    fix test by returning to global but with patch

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,4 @@
-import '@testing-library/jest-dom/extend-expect';
-import { describe, it, expect } from 'vitest';
+import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { WrappedApp, App } from './App';
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
   },
