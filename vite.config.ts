e439238b04bcd5b9fbf5a1d73fcce3908b4418ec
Swann HERRERA <swann@devloup.dev>
2022-11-22 17:08:48 +0100

    fix: use npm run instead of npm

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   test: {
     globals: true,
     environment: 'jsdom',
