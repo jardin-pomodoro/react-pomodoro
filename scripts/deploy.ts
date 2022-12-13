23da9ab63e11eec2d9957500b6dfae2c7ce145ba
Nouuu <nlarrieulacoste@myges.fr>
2022-12-13 18:44:09 +0100

    feat(contract): enable deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,37 +1,45 @@
-import { artifacts, ethers } from 'hardhat';
-import { Contract } from '@ethersproject/contracts';
+import {artifacts, ethers} from 'hardhat';
+import {Contract} from '@ethersproject/contracts';
 import * as fs from 'fs';
 import * as path from 'path';
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
+    const [deployer] = await ethers.getSigners();
 
-  console.log('Deploying contracts with the account:', deployer.address);
-  console.log('Account balance:', (await deployer.getBalance()).toString());
+    console.log('Deploying contracts with the account:', deployer.address);
+    console.log('Account balance:', (await deployer.getBalance()).toString());
 
-  const contractFactory = await ethers.getContractFactory('GardenToken');
-  const gardenTokenContract = await contractFactory.deploy();
+    const contractFactory = await ethers.getContractFactory('GardenToken');
+    const gardenTokenContract = await contractFactory.deploy();
 
-  console.log('Garden Token address:', gardenTokenContract.address);
+    console.log('Garden Token address:', gardenTokenContract.address);
 
-  saveFrontendDeploymentInfo(gardenTokenContract);
+    saveFrontendDeploymentInfo(gardenTokenContract);
 }
 
 function saveFrontendDeploymentInfo(contract: Contract) {
-  const frontendDir = __dirname + '../frontend/src/contracts';
+    const frontendDir = __dirname + '../frontend/src/contracts';
 
-  if (!fs.existsSync(frontendDir)) {
-    fs.mkdirSync(frontendDir);
-  }
+    if (!fs.existsSync(frontendDir)) {
+        fs.mkdirSync(frontendDir);
+    }
 
-  fs.writeFileSync(
-    path.join(frontendDir, 'contract-address.json'),
-    JSON.stringify({ Token: contract.address }, undefined, 2),
-  );
+    fs.writeFileSync(
+        path.join(frontendDir, 'contract-address.json'),
+        JSON.stringify({Token: contract.address}, undefined, 2),
+    );
 
-  const contractArtifact = artifacts.readArtifactSync('GardenToken');
-  fs.writeFileSync(
-    path.join(frontendDir, 'GardenToken.json'),
-    JSON.stringify(contractArtifact, null, 2),
-  );
+    const contractArtifact = artifacts.readArtifactSync('GardenToken');
+    fs.writeFileSync(
+        path.join(frontendDir, 'GardenToken.json'),
+        JSON.stringify(contractArtifact, null, 2),
+    );
 }
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
